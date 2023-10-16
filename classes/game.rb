77ea8639098e34e7e649d249b63f7b02b66094d7
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-10-17 00:28:37 +0200

    Add the Game class with constructor properties and required method

---
+++ classes/game.rb
@@ -0,0 +1,18 @@
+require_relative 'item'
+require 'date'
+
+class Game < Item
+  attr_accessor :multiplayer, :last_played_at
+
+  def initialize(publish_date, multiplayer, last_played_at)
+    super(publish_date)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
+
+  private
+
+  def can_be_archived?
+    super && Date.today.prev_year(2) > Date.parse(@last_played_at)
+  end
+end
