3bccd46ed16b83fe4bde922595232614f5186471
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-10-16 19:34:19 +0200

    Create the add_author method

--- classes/item.rb
+++ classes/item.rb
@@ -1,16 +1,20 @@
 class Item
+  attr_accessor :author, :label, :publish_date
 
-def initialize(pubish_date)
-  @id = Random.rand(1..1000)
-  @label  = nil
-  @publish_date = pubish_date
-  @archived = archived = false
-end
+  def initialize(pubish_date)
+    @id = Random.rand(1..1000)
+    @author = nil
+    @label = nil
+    @publish_date = pubish_date
+    @archived = archived = false
+  end
 
-def add_label(title)
-  @label  = label
-  my_label = label.new(title)
-end
+  def add_label(title)
+    @label = label.new(title)
+  end
 
-
+  def add_author(author)
+    @author = author
+    author.item.push(self) unless author.items.include?(self)
+  end
 end    
