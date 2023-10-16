b119ba8da959bd3532a693e3cada8d11543f5d2d
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-10-16 14:08:40 -0300

    change author in Item to label

--- classes/item.rb
+++ classes/item.rb
@@ -2,14 +2,14 @@ class Item
 
 def initialize(pubish_date)
   @id = Random.rand(1..1000)
-  @author  = nil
+  @label  = nil
   @publish_date = pubish_date
   @archived = archived = false
 end
 
-def add_author(first_name, last_name)
-  @author  = author
-  my_author = Author.new(first_name, last_name)
+def add_label(title)
+  @label  = label
+  my_label = label.new(title)
 end
 
 
