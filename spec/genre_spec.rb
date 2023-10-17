e46b7bdff54474a46315d0d5aa418a52361684db
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-10-17 17:04:06 -0500

    Test if it throws an ArgumentError when creating a genre incorrectly

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -8,4 +8,10 @@ describe Genre do
       expect(genre.name).to eq('Salsa')
     end
   end
+
+  context 'when creating a new genre with no parameters' do
+    it 'throws an ArgumentError when given fewer than 1 parameter' do
+      expect { Genre.new }.to raise_exception ArgumentError
+    end
+  end
 end
