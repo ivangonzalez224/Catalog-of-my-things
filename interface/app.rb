c71bc32e3c930457f873ba7223891a696260d4b2
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-10-17 13:40:55 -0300

    create CreateItems module - add enter_date and add_book methods

--- interface/app.rb
+++ interface/app.rb
@@ -1,84 +1,116 @@
-require_relative 'modules/genres_operation'
+require_relative 'modules/add_items'
 
 class App
-
-def initialize
+    include CreateItems
+  def initialize
     @items = []
     @genres = []
     @authors = []
-    @labels = [] 
+    @labels = []
     @books = []
     @games = []
     @music_album = []
-end
+  end
 
-def show_menu
-    puts '\nSelect an option by number:'
+  def run
+    puts 'Welcome!'
+    loop do
+        operation0
+    end
+  end
+
+  def show_menu
+    puts " \n Select an option by number:"
     puts '1) List items (books, music albums or games)'
     puts '2) List categories (genres, labels or authors)'
-    puts '3) Add items (book, music album or game)' 
-end    
+    puts '3) Add items (book, music album or game)'
+    puts '4) Exit'
+  end
 
-
-def run
+  def operation0
     show_menu
-    user_input = gets.chomp.to_i 
+    user_input = gets.chomp.to_i
     case user_input
-    when 1: operation1  
-    when 2: operation2     
-    when 3: operation3      
-end   
+    when 1
+      operation1
+    when 2
+      operation2
+    when 3
+      operation3
+    when 4
+        exit_app
+    else
+      puts 'Invalid option'
+    end
+  end
 
-def operation1
-    puts '1. List all books'
-    puts '2. List all music albums' 
-    puts '3. List of games'
+  def operation1
+    puts " \n Select an option by number:"
+    puts '1) List all books'
+    puts '2) List all music albums'
+    puts '3) List of games'
+    puts '4) Go back Main to Menu'
     option = gets.chomp.to_i
     case option
     when 1
-        list_all_books()
+      list_all_books
     when 2
-        list_all_music_albums()
+      list_all_music_albums
     when 3
-        list_games() 
-    else     
-        puts 'Invalid option'
-    end       
-     
-end    
+      list_games
+    when 4
+        operation0
+    else
+      puts 'Invalid option'
+    end
+  end
 
-def operation2
-    puts '1. List all genres'
-    puts '2. List all labels' 
-    puts '3. List of authors'
+  def operation2
+    puts " \n Select an option by number:"
+    puts '1) List all genres'
+    puts '2) List all labels'
+    puts '3) List of authors'
+    puts '4) Go back Main to Menu'
     option = gets.chomp.to_i
     case option
     when 1
-        list_all_genres()
+      list_all_genres
     when 2
-        list_all_labels()
+      list_all_labels
     when 3
-        list_all_authors()
-    else     
-        puts 'Invalid option'
-    end        
+      list_all_authors
+    when 4
+        operation0
+    else
+      puts 'Invalid option'
+    end
+  end
 
-end    
-
-def operation3
-    puts '1. Add a book'
-    puts '2. Add a music album' 
-    puts '3. Add a game'
+  def operation3
+    puts " \n Select an option by number:"
+    puts '1) Add a book'
+    puts '2) Add a music album'
+    puts '3) Add a game'
+    puts '4) Go back Main to Menu'
     option = gets.chomp.to_i
     case option
     when 1
-        add_book()
+      myBook = add_book
+      @books << myBook
     when 2
-        add_music_album()
+      add_music_album
     when 3
-        add_game()
-    else     
-        puts 'Invalid option'
-    end    
-end 
+      add_game
+    when 4
+        operation0
+    else
+      puts 'Invalid option'
+    end
+  end
+
+  def exit_app 
+    puts 'Thank you for using the app!'
+
+    exit
+  end
 end
---
+++ interface/modules/add_items.rb
@@ -0,0 +1,19 @@
+require './classes/book'
+
+module CreateItems 
+
+  def enter_date
+    print 'Enter publish date: '
+    publish_date = gets.chomp
+  end
+
+  def add_book
+    publish_date = enter_date
+    print 'Enter the book publisher: '
+    publisher = gets.chomp
+    print 'Enter the book cover state: '
+    cover_state = gets.chomp
+    newBook = Book.new(publish_date, publisher, cover_state)
+    puts 'Your book has been created'
+  end
+end
