3a415aac89468587e86331ca1337fac9ccec7cc0
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-10-16 22:15:51 -0500

    Create the MusicAlbum table

--- schema.sql
+++ schema.sql
@@ -3,3 +3,11 @@ Create table Genre (
   name varchar(100),
   PRIMARY KEY (id)
 );
+
+Create table MusicAlbum (
+  id int GENERATED ALWAYS AS IDENTITY,
+  item_id int,
+  on_spotify BOOLEAN,
+  CONSTRAINT FK_MUSICALBUM_ITEM_ID FOREIGN KEY (item_id) REFERENCES Item(id),
+  PRIMARY KEY (id) 
+);
