23fe7d09a9ccdfc03c089dd345ca4ec9f2509e1b
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-10-17 12:16:24 -0300

    fix linters issues

--- classes/book.rb
+++ classes/book.rb
@@ -3,7 +3,7 @@ require_relative 'item'
 class Book < Item
   attr_accessor :publisher, :cover_state
 
-  def initialize(publish_date, publisher, cover_state, id = Random.rand(1..1000))
+  def initialize(publish_date, publisher, cover_state, _id = Random.rand(1..1000))
     super(publish_date)
     @cover_state = cover_state
     @publisher = publisher
