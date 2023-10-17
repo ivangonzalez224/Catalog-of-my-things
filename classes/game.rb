af301732a2f66ce3817903dde4cfeccbdda3a893
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-10-17 19:12:22 +0200

    Check if game is working with an example

--- classes/game.rb
+++ classes/game.rb
@@ -4,15 +4,19 @@ require 'date'
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
-  def initialize(publish_date, multiplayer, last_played_at)
+  def initialize(publish_date, multiplayer, last_played_at, _id = Random.rand(1..1000))
     super(publish_date)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
 
-  private
-
   def can_be_archived?
     super && Date.today.prev_year(2) > Date.parse(@last_played_at)
   end
 end
+
+game1 = Game.new('2004/10/04', 'true', '2019/03/15')
+puts game1.publish_date
+puts game1.multiplayer
+puts game1.can_be_archived?
+puts game1.id
