4500ae1da4af6dc19be414bdd7de2417f3c86414
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-10-17 17:40:59 -0500

    Test if a new music album has the on_spotify attribute

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -4,8 +4,13 @@ require './classes/item'
 describe MusicAlbum do
   context 'when creating a new genre' do
     it 'should have a publish_date' do
-      music_album = MusicAlbum.new('2023-08-04', true)
-      expect(music_album.publish_date).to eq('2023-08-04')
+      music_album = MusicAlbum.new('2022-12-04', true)
+      expect(music_album.publish_date).to eq('2022-12-04')
+    end
+
+    it 'should have an on_spotify attribute' do
+      music_album = MusicAlbum.new('2022-12-04', true)
+      expect(music_album.on_spotify).to be true
     end
   end
 end
