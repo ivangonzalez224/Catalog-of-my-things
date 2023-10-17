e19295e0a8e85c8a2d6fa08c24027f8a543a7fdc
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-10-16 23:39:55 -0300

    add Label class

---
+++ classes/label.rb
@@ -0,0 +1,13 @@
+class Label
+  attr_accessor :items, :title, :color
+  def initialize(title, color)
+    @id = Random.rand(1..1000)
+    @title = title
+    @color = color
+    @items = []
+  end
+  def add_item(item)
+    @items << (item)
+    item.label = self
+  end
+end
