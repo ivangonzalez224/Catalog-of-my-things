75e6542d877c0a62b3bf2df93f8875b4a412d6bb
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-10-18 00:53:51 -0300

    add if statemetn to list_all_books method

--- interface/modules/list_items.rb
+++ interface/modules/list_items.rb
@@ -7,9 +7,13 @@ module ListItems
   end
 
   def list_all_books
-    puts 'Books:'
-    @books.each_with_index do |book, index|
-      puts "#{index + 1}) ID: #{book.id}, Publish date: #{book.publish_date}, Cover State: #{book.cover_state}"
+    if @books.empty? 
+      puts 'There are no books yet.'
+    else
+      puts 'Books:'
+      @books.each_with_index do |book, index|
+        puts "#{index + 1}) ID: #{book.id}, Publish date: #{book.publish_date}, Cover State: #{book.cover_state}"
+      end
     end
   end
 end
