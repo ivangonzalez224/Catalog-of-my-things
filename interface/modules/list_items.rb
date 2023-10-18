f771c0651a18a0be1ff7d3840cdf87bd8eab8fa3
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-10-17 20:22:14 -0500

    Add the option and method to list all genres

--- interface/modules/list_items.rb
+++ interface/modules/list_items.rb
@@ -10,4 +10,15 @@ module ListItems
       end
     end
   end
+
+  def list_all_genres
+    if @genres.empty?
+      puts 'The Genres list is empty.'
+    else
+      puts 'Genres:'
+      @genres.each_with_index do |genre, index|
+        puts "#{index + 1}) ID: #{genre.id}, Name: #{genre.name}"
+      end
+    end
+  end
 end
