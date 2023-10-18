825f58a34fa8600410676b92bdfcc7f4562d318c
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-10-18 13:34:31 +0200

    Create list_all_authors method to list them

--- interface/modules/list_categories.rb
+++ interface/modules/list_categories.rb
@@ -9,4 +9,15 @@ module ListCategories
       end
     end
   end
+
+  def list_all_authors
+    if @authors.empty?
+      puts 'The Authors list is empty.'
+    else
+      puts 'Authors:'
+      @authors.each_with_index do |author, index|
+        puts "#{index + 1}) ID: #{author.id}, First name: #{author.first_name}, Last name: #{author.last_name}"
+      end
+    end
+  end
 end
